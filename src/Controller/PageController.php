d09ddb4443e5a7c0d175bfed951dced78ca23efa
Xope <errchosep25@gmail.com>
2022-12-02 13:12:49 +0100

    Afegit controlador embebido

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Team;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,39 +16,46 @@ class PageController extends AbstractController
         return $this->render('page/index.html.twig', []);
     }
 
-    #[Route('/about', name:'about')]
-    public function about(): Response
+    #[Route('/about', name: 'about')]
+    public function about(ManagerRegistry $doctrine): Response
     {
         return $this->render('page/about.html.twig', []);
     }
 
-    #[Route('/contact', name:'contact')]
+    #[Route('/contact', name: 'contact')]
     public function contact(): Response
     {
         return $this->render('page/contact.html.twig', []);
     }
 
-    #[Route('/pricing', name:'pricing')]
+    #[Route('/pricing', name: 'pricing')]
     public function pricing(): Response
     {
         return $this->render('page/price.html.twig', []);
     }
 
-    #[Route('/team', name:'team')]
+    #[Route('/team', name: 'team')]
     public function team(): Response
     {
         return $this->render('page/team.html.twig', []);
     }
 
-    #[Route('/service', name:'service')]
+    #[Route('/service', name: 'service')]
     public function service(): Response
     {
         return $this->render('page/service.html.twig', []);
     }
 
-    #[Route('/testimonial', name:'testimonial')]
+    #[Route('/testimonial', name: 'testimonial')]
     public function testimonial(): Response
     {
         return $this->render('page/testimonial.html.twig', []);
+    }
+
+    public function teamTemplate(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Team::class);
+        $team = $repository->findAll();
+        return $this->render('partials/_team.html.twig', compact('team'));
     }
 }
